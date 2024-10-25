bef6060223d3a9b8f899403e86571e647b09c09a
Phoenix Himself <pkaras.it@gmail.com>
2024-10-25 17:19:31 +0200

    5

--- task5.py
+++ task5.py
@@ -1,3 +1,4 @@
+import array
 
 def read_file(input):
     with open(input, 'r') as f:
@@ -5,6 +6,12 @@ def read_file(input):
         rooms = int(lines[0])
         for room in lines[1:]:
             (x, y, n) = map(int, room.split())
+
+            reversed = False
+            if x < y:
+                (x, y) = (y, x)
+                reversed = True
+
             matrix = [['.' for i in range(x)] for j in range(y)]
             last = 0
 
@@ -14,12 +21,17 @@ def read_file(input):
                     matrix[i][j+1] = 'X'
                     last = j
 
-
             z = x - (x % 3)
             for j in range(last + 3, x, 2):
                 for i in range(0, y-1, 3):
                     matrix[i][j] = 'X'
                     matrix[i+1][j] = 'X'
+
+
+
+            if reversed:
+                matrix = [list(row) for row in zip(*matrix)]
+                (y, x) = (x, y)
 
             for i in range(y):
                 for j in range(x):
