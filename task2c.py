6cada9da14c5080b601b8aeb15a12705871b4863
mckk12 <maciej12ciepiela@gmail.com>
2024-10-25 15:26:27 +0200

    fix: task2

--- task2c.py
+++ task2c.py
@@ -5,13 +5,13 @@ def read_file(input):
         rooms = int(lines[0])
         for room in lines[1:]:
             (x, y, n) = room.split()
-        h = 1
-        for i in range(y):
-            for j in range(int (x / 3)):
-                print(h, h, h, end=' ')
-                h+=1
-            print()
+            h = 1
+            for i in range(int(y)):
+                for j in range(int( int(x) / 3)):
+                    print(h, h, h, end=' ')
+                    h+=1
+                print("\n")
 
 
 if __name__ == '__main__':
-    read_file('level1_5.in.txt')
+    read_file('level2_1.in.txt')
